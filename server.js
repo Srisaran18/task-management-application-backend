3071ceb05ee71ac1d4ee46efdf179295c70d88c4
Sridhar Vellingiri <sridhar@braincert.com>
2025-09-15 21:49:06 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:3000", credentials: false }));
+app.use(cors({ origin: ["http://localhost:3000", "task-management-application-fronten.vercel.app"], credentials: false }));
 app.use(express.json());
 
 mongoose
