02e6eb969b89f823ba76f9f1a8b7a6ab525fce1a
Sridhar Vellingiri <sridhar@braincert.com>
2025-09-15 22:25:27 +0530

    error fixed

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "http://127.0.0.1:3000",
-  "https://task-management-application-frontend.vercel.app"
+  "https://task-management-application-fronten.vercel.app"
 ];
 
 app.use(cors({
